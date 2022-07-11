5c6a8ac31d9d99659b4c2188591337ace0afa24b
artigon <itay.b31@gmail.com>
2022-07-11 14:45:53 +0300

    add npc - not yet working

---
+++ Assets/scripts/soldierNpcMec.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class soldierNpcMe : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public GameObject target;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
