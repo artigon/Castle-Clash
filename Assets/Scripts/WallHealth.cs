a7101f7626cd72b50aa4251de35d2f6587fd7273
IdoS88 <ishamir99@gmail.com>
2022-07-18 21:29:55 +0300

    WallHealth Script Update

--- Assets/Scripts/WallHealth.cs
+++ Assets/Scripts/WallHealth.cs
@@ -8,84 +8,120 @@ public class WallHealth : MonoBehaviour
     public int health;
     public bool isFrontGate;
     private GameObject gate;
-    private GameObject wall;
-    private GameObject rubble;
-    private GameObject ruinedWall;
+    private List<GameObject> walls;
+    private List<GameObject> rubbles;
+    private List<GameObject> ruinedWalls;
     public bool isSemiRuined;
     public bool isWrecked;
     public FortWallsWarning warningScript;
     // Start is called before the first frame update
     void Start()
-    {
-
-        wall = transform.GetChild(0).gameObject;
-        ruinedWall = transform.GetChild(1).gameObject;
-        rubble = transform.GetChild(2).gameObject;
+    { 
+        walls = new List<GameObject>();
+        rubbles = new List<GameObject>();
+        ruinedWalls = new List<GameObject>();
+        setListGameObjects(walls, 0); // index of son
+        setListGameObjects(ruinedWalls, 1); // index of son
+        setListGameObjects(rubbles, 2); // index of son
         if (isFrontGate)
-            gate = transform.GetChild(3).gameObject;
-        
+            gate = transform.Find("FrontGateHP").GetChild(3).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(isWrecked)
+        //if (isWrecked)
         //{
-        //    if (transform.parent.name == "FrontWalls")
+        //    if (isFrontGate)
         //        warningScript.setWarning("front");
-        //    else if (transform.parent.name == "RightSideWalls")
+        //    else if (gameObject.name == "LeftSideWalls")
+        //        warningScript.setWarning("left");
+        //    else if (gameObject.name == "RightSideWalls")
         //        warningScript.setWarning("right");
         //    else
-        //        warningScript.setWarning("left");
+        //        warningScript.setWarning("back"); 
         //}
-            
+
         if (health < 40 && health > 0 && !isSemiRuined)
         {
-            StartCoroutine(setWalls(ruinedWall));
+            StartCoroutine(setWalls(getActiveObject(),1)); // 1 = ruinedWalls new state
             isSemiRuined = true;
             isWrecked = false;
         }
         else
         if (health <= 0)
         {
-            StartCoroutine(setWalls(rubble));
+            StartCoroutine(setWalls(getActiveObject(),2)); // 2 = rubbles new state
             isWrecked = true;
             isSemiRuined = false;
         }
           
-        IEnumerator setWalls(GameObject changedWall)
+        IEnumerator setWalls(int activeIndex, int newIndex)
         {
             yield return new WaitForSeconds(0.5f);
-            if (isFrontGate)
+            switch (activeIndex)
             {
-                if (getActiveChild() == wall)
-                {
-                    wall.SetActive(false);
-                    gate.SetActive(false);
-                }
-                else
-                {
-                    getActiveChild().SetActive(false);
-                }
-                changedWall.SetActive(true);
+                case 0: //  regular Wall
+                    {
+                        for (int i = 0; i < walls.Count; i++)
+                            walls[i].SetActive(false);
+                        if (isFrontGate)
+                            gate.SetActive(false);
+                    }
+                    break;
+                case 1: // ruined wall
+                    {
+                        for (int i = 0; i < ruinedWalls.Count; i++)
+                            ruinedWalls[i].SetActive(false);
+                    }
+                    break;
+                case 2: // rubble
+                    {
+                        for (int i = 0; i < rubbles.Count; i++)
+                            rubbles[i].SetActive(false);
+                    }
+                    break;
             }
-            else
+            switch (newIndex)
             {
-                getActiveChild().SetActive(false);
-                changedWall.SetActive(true);
+                case 0:  //  regular Wall
+                    {
+                        for (int i = 0; i < walls.Count; i++)
+                            walls[i].SetActive(true);
+                        if (isFrontGate)
+                            gate.SetActive(true);
+                    }
+                    break;
+                case 1: // ruined wall
+                    {
+                        for (int i = 0; i < ruinedWalls.Count; i++)
+                            ruinedWalls[i].SetActive(true);
+                    }
+                    break;
+                case 2: // rubble
+                    {
+                        for (int i = 0; i < rubbles.Count; i++)
+                            rubbles[i].SetActive(true);
+                    }
+                    break;
             }
-        }
+        }      
     }
-    public GameObject getActiveChild()
+    public int getActiveObject()
     {
-        for(int i = 0; i < gameObject.transform.childCount;i++)
-        {
-            if (gameObject.transform.GetChild(i).gameObject.activeSelf == true)
+            for (int i = 0; i < transform.GetChild(0).childCount; i++)
             {
-                return gameObject.transform.GetChild(i).gameObject;
+                if (transform.GetChild(0).GetChild(i).gameObject.activeSelf)
+                    return i;
             }
-        }
-        return null;
+        
+        return -1;
+    }
+    public void setListGameObjects(List<GameObject> l, int childIndex)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+            l.Add(transform.GetChild(i).GetChild(childIndex).gameObject);
+        
     }
 
     public void takeDamege(int damege)
