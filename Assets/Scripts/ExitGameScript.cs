f078b0b12aec00510afde2d75f027e1e5f5345de
OhadLayani <103877865+OhadLayani@users.noreply.github.com>
2022-07-26 17:15:06 +0300

    MainMenu changes
    seperated panels into a main menu scene and game scene , added exite screen to be assigned.

---
+++ Assets/Scripts/ExitGameScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void exitgame()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+}  
+
---
+++ Assets/Scripts/SceneMovementScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SceneMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneMovement()
+    {
+        print("Button Is clicked");
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+       // StartCoroutine(SceneSwitch());//run in parallel
+    }
+    IEnumerator SceneSwitch()
+    {
+        //play fade in animation
+        //delay
+        yield return new WaitForSeconds(2);
+        //SceneSwitch
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
